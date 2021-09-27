d2a7e76324f2245911a76ee118bc19bf239529af
Luis Fernando Vidal <lvidaldev@gmail.com>
2021-09-27 02:28:21 -0500

    initial commit

---
+++ src/main/java/com/writecode/alcances/AlcancesApplication.java
@@ -0,0 +1,13 @@
+package com.writecode.alcances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcancesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlcancesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/writecode/alcances/AlcancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.writecode.alcances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcancesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
